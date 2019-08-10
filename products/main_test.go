73fad11a6d39087770ff516c271826cba6ec9e58
arosa822 <h.rosa822@gmail.com>
2019-08-10 07:36:10 -0600

    adding test cases for fetch and update

--- products/main_test.go
+++ products/main_test.go
@@ -1,13 +1,14 @@
 package main_test
 
 import (
+	"."
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
-
-	"."
 )
 
 var a main.App
@@ -73,5 +74,51 @@ func executeRequest(req *http.Request) *httptest.ResponseRecorder {
 func checkResponseCode(t *testing.T, expected, actual int) {
 	if expected != actual {
 		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func TestGetNonExistedntProduct(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/product/11", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "Product not found" {
+		t.Errorf("Expected the 'error' key of the response to"+
+			"'Product not found'. got '%s'", m["error"])
+	}
+}
+
+func TestCreateProduct(t *testing.T) {
+	clearTable()
+
+	payload := []byte(`{"name":"test product","price":11.22}`)
+
+	req, _ := http.NewRequest("POST", "/product", bytes.NewBuffer(payload))
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] != "test product" {
+		t.Errorf("Expected product name to be 'test product'"+
+			"Got '%v'", m["name"])
+	}
+
+	if m["price"] != 11.22 {
+		t.Errorf("Expected product price to be '11.22'"+
+			"Got '%v'", m["price"])
+	}
+
+	// the id is compared to 1.0 because JSON unmarshaling convers
+	// numbers to floats, when the target is a map[string]interface{}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected product ID to be '1'. Got '%v'", m["id"])
 	}
 }
